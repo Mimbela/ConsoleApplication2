2ebd80521d992f6fd70385f0d605cd450485d183
senin8 <malizziaaries@gmail.com>
2018-05-15 10:32:53 +0200

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadenaConexion = @"Data Source=DESKTOP-7KN5JV1\SQLEXPRESS;Initial Catalog=NORTHWND;Integrated Security=True";
+            string query = "select * from employees";
+            using (SqlConnection objConexion = new SqlConnection(cadenaConexion))
+            {
+                objConexion.Open();
+                SqlDataAdapter daEmpleados = new SqlDataAdapter(query, objConexion);
+
+                DataSet dsEmpresa = new DataSet("Empresa");
+                daEmpleados.Fill(dsEmpresa, "Empleados");
+
+                DataTable tblEmpleados = dsEmpresa.Tables["Empleados"];
+
+                foreach (DataRow drCurrent in tblEmpleados.Rows)
+                {
+                    Console.WriteLine("{0} - {1} - {2} ", drCurrent [0] , drCurrent[1] , drCurrent [2]);
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
